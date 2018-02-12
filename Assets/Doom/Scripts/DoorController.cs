8f5b9dcf55caaaebc529ce4e61efccbdf4ee94ac
Luke Davies <lkdvs3@gmail.com>
2018-02-12 17:48:29 +0000

    Added simple sliding door prefab

---
+++ Assets/Doom/Scripts/DoorController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Animator))]
+public class DoorController : MonoBehaviour
+{
+    #region Unity Bindings
+
+    public bool m_open;
+
+    #endregion
+
+    #region Private Fields
+
+    Animator _animator;
+
+    #endregion
+
+    #region MonoBehaviour
+
+    void Awake()
+    {
+        _animator = gameObject.GetComponent<Animator>();
+    }
+
+    void Start()
+    {
+        if (m_open)
+        {
+            _animator.Play("Open");
+            _animator.SetBool("IsOpen", true);
+        }
+    }
+
+    void DoActivateTrigger()
+    {
+        _animator.SetBool("IsOpen", !_animator.GetBool("IsOpen"));
+    }
+
+    #endregion
+}
