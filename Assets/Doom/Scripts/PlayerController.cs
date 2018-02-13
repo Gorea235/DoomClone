78bf4947e6bd141134f42593bd82673f108aef4d
Luke Davies <lkdvs3@gmail.com>
2018-02-13 01:13:48 +0000

    Added health bar

--- Assets/Doom/Scripts/PlayerController.cs
+++ Assets/Doom/Scripts/PlayerController.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 [RequireComponent(typeof(AudioSource))]
 public class PlayerController : MonoBehaviour
@@ -6,14 +7,20 @@ public class PlayerController : MonoBehaviour
     #region Unity Bindings
 
     public AudioClip[] m_hurtSounds;
-    public float m_health;
+    public float m_maxHealth;
+    public Mask m_healthBarMask;
+    public Text m_HealthPercentText;
 
     #endregion
 
     #region Private Fields
 
+    const string healthTextFormat = "{0:P}";
+
     DataStore _dataStorage;
     AudioSource _audioSource;
+    float _health;
+    Vector2 _defaultHealthBarMaskSize;
 
     #endregion
 
@@ -23,17 +30,35 @@ public class PlayerController : MonoBehaviour
     {
         _dataStorage = GameObject.Find("DataStore").GetComponent<DataStore>();
         _audioSource = gameObject.GetComponent<AudioSource>();
+        _defaultHealthBarMaskSize = m_healthBarMask.rectTransform.sizeDelta;
+        _health = m_maxHealth;
+    }
+
+    void Start()
+    {
+        UpdateHealthBar();
     }
 
     #endregion
 
     #region Helper Methods
 
+    void UpdateHealthBar()
+    {
+        float percent = (_health / m_maxHealth);
+        m_healthBarMask.rectTransform.sizeDelta = new Vector2(_defaultHealthBarMaskSize.x * percent,
+                                                              _defaultHealthBarMaskSize.y);
+        m_HealthPercentText.text = string.Format(healthTextFormat, percent);
+    }
+
     public void Damage(float amount)
     {
-        m_health -= amount;
-        Debug.Log(m_health);
-        if (m_health <= 0)
+        // update health
+        _health -= amount;
+        // update UI
+        UpdateHealthBar();
+        // check if failed, otherwise play hurt sound
+        if (_health <= 0)
         {
             Debug.Log("failed game");
             _dataStorage.SetSucceeded(false);
