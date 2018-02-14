e81cf7d336b2122231310a2b6d23c0cda2c8b934
Luke Davies <lkdvs3@gmail.com>
2018-02-14 19:23:26 +0000

    Added door sounds

--- Assets/Doom/Scripts/Util/DoorController.cs
+++ Assets/Doom/Scripts/Util/DoorController.cs
@@ -1,17 +1,21 @@
 ﻿using UnityEngine;
 
 [RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(AudioSource))]
 public class DoorController : MonoBehaviour
 {
     #region Unity Bindings
 
-    public bool m_open;
+    public bool m_startOpen;
+    public AudioClip m_openSound;
+    public AudioClip m_closeSound;
 
     #endregion
 
     #region Private Fields
 
     Animator _animator;
+    AudioSource _audioSource;
 
     #endregion
 
@@ -20,11 +24,12 @@ public class DoorController : MonoBehaviour
     void Awake()
     {
         _animator = gameObject.GetComponent<Animator>();
+        _audioSource = gameObject.GetComponent<AudioSource>();
     }
 
     void Start()
     {
-        if (m_open)
+        if (m_startOpen)
         {
             _animator.Play("Open");
             _animator.SetBool("IsOpen", true);
@@ -34,6 +39,22 @@ public class DoorController : MonoBehaviour
     void DoActivateTrigger()
     {
         _animator.SetBool("IsOpen", !_animator.GetBool("IsOpen"));
+    }
+
+    #endregion
+
+    #region Helper Methods
+
+    internal void PlayOpenSound()
+    {
+        _audioSource.clip = m_openSound;
+        _audioSource.Play();
+    }
+
+    internal void PlayCloseSound()
+    {
+        _audioSource.clip = m_closeSound;
+        _audioSource.Play();
     }
 
     #endregion
